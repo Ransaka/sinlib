80e7c5871f4c65af7546bf962c4d3b76be5bcbce
ranzaka <ransaka.withanachchi@prodoscore.com>
2024-02-11 00:04:43 +0530

    v.0.0.8 hot fix

--- src/sinlib/__init__.py
+++ src/sinlib/__init__.py
@@ -1,10 +1,9 @@
 from sinlib.tokenizer import Tokenizer
-from sinlib.utils.preprocessing import remove_english_characters, remove_non_printable, retain_sinhala_characters, get_sinhala_token_percentage
+from sinlib.utils.preprocessing import remove_english_characters, remove_non_printable, get_sinhala_token_percentage
 
 __all__ = [
     "Tokenizer",
     "remove_english_characters",
     "remove_non_printable",
-    "retain_sinhala_characters",
     "get_sinhala_token_percentage"
 ]
