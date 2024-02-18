283595c47c8529bea6a1713bbab6fd789ec09315
ranzaka <ransaka.withanachchi@prodoscore.com>
2024-02-18 12:24:35 +0530

    bug fixing

--- src/sinlib/utils/__init__.py
+++ src/sinlib/utils/__init__.py
@@ -1,4 +1,4 @@
-from utils.preprocessing import remove_english_characters, remove_non_printable, get_sinhala_character_ratio
+from .preprocessing import remove_english_characters, remove_non_printable, get_sinhala_character_ratio
 
 __all__ = [
     "remove_english_characters",
