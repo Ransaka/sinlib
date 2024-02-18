10e6ef1d5b7b2965af2fe6659bc164c8c8fc6411
ranzaka <ransaka.withanachchi@prodoscore.com>
2024-02-18 12:15:35 +0530

    changed preprocessing methods

--- src/sinlib/utils/__init__.py
+++ src/sinlib/utils/__init__.py
@@ -1,7 +1,7 @@
-from utils.preprocessing import remove_english_characters, remove_non_printable, get_sinhala_character_percentage
+from utils.preprocessing import remove_english_characters, remove_non_printable, get_sinhala_character_ratio
 
 __all__ = [
     "remove_english_characters",
     "remove_non_printable",
-    "get_sinhala_character_percentage"
+    "get_sinhala_character_ratio"
 ]
--- src/sinlib/utils/preprocessing.py
+++ src/sinlib/utils/preprocessing.py
@@ -9,9 +9,11 @@ import os
 # with open(file_path,'r') as f:
 #     SINHALA_CHARS_WITH_SPECIAL_CHARS = f.read().split("\n")
 
+
 def remove_non_printable(input_string):
-    printable_pattern = re.compile(r'[^\u0020-\u007E\u0D80-\u0DFF]+', flags=re.UNICODE)
-    return printable_pattern.sub('', input_string)
+    printable_pattern = re.compile(r"[^\u0020-\u007E\u0D80-\u0DFF]+", flags=re.UNICODE)
+    return printable_pattern.sub("", input_string)
+
 
 def remove_english_characters(text):
     """
@@ -24,9 +26,10 @@ def remove_english_characters(text):
     - str: Text with English characters removed.
     """
     english_pattern = re.compile("[a-zA-Z]")
-    text = english_pattern.sub(r'', text)
-    text = re.sub(r'\s+', ' ', text).strip()
+    text = english_pattern.sub(r"", text)
+    text = re.sub(r"\s+", " ", text).strip()
     return text
+
 
 # def retain_sinhala_characters(text):
 #     """
@@ -50,7 +53,7 @@ def process_text(t):
             continue
         if char in NUBERS_AND_PUNKTS:
             tokenized_chars.append(char)
-        elif char == ' ':
+        elif char == " ":
             tokenized_chars.append(" ")
         elif char in ALL_LETTERS:
             if i < len(t) - 1 and t[i + 1] in ALL_LETTERS:
@@ -65,17 +68,17 @@ def process_text(t):
     return tokenized_chars
 
 
-def process_text_with_token_counts(t):
+def process_text_with_token_counts(t, consider_special_character_as_sinhala):
     tokenized_chars = []
     token_counts = 0
 
     for i, char in enumerate(t):
         if char in VOWEL_DIACRITICS:
             continue
-        if char in NUBERS_AND_PUNKTS:
+        if (char in NUBERS_AND_PUNKTS) and (consider_special_character_as_sinhala):
             tokenized_chars.append(char)
             token_counts += 1
-        elif char == ' ':
+        elif char == " ":
             tokenized_chars.append(" ")
         elif char in ALL_LETTERS:
             token_counts += 1
@@ -85,16 +88,19 @@ def process_text_with_token_counts(t):
                 tokenized_chars.append(char + t[i + 1])
             else:
                 tokenized_chars.append(char)
-                
+
         else:
             tokenized_chars.append(char)
 
-    return tokenized_chars,token_counts
+    return tokenized_chars, token_counts
 
 
-def get_sinhala_character_percentage(text):
+def get_sinhala_character_ratio(text, consider_special_character_as_sinhala:bool=True, ignore_non_printable:bool=True):
+    """Retuning sinhala character ratio for given text string for given settings."""
+    if ignore_non_printable:
+        text = remove_non_printable(text)
     if isinstance(text, str):
-        tokenized_text, sinhala_token_count = process_text_with_token_counts(text)
+        tokenized_text, sinhala_token_count = process_text_with_token_counts(text,consider_special_character_as_sinhala)
         tokenized_text = [tok for tok in tokenized_text if tok != " "]
         return sinhala_token_count / len(tokenized_text)
     elif isinstance(text, list):
@@ -103,6 +109,6 @@ def get_sinhala_character_percentage(text):
         pool.close()
         pool.join()
         encodings = [tok[0] for tok in results if tok[0] != " "]
-        encodings = [[char for char in enc if char!=" "] for enc in encodings]
+        encodings = [[char for char in enc if char != " "] for enc in encodings]
         sinhala_lengths = [tok[1] for tok in results]
-        return [(l/len(enc)) for enc,l in zip(encodings,sinhala_lengths )]
+        return [(l / len(enc)) for enc, l in zip(encodings, sinhala_lengths)]
